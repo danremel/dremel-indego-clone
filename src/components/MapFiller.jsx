7b1eed3347a9a88995bfe72ed25edf7edc9d97ac
danremel <remel.dan@gmail.com>
2018-03-13 14:07:52 -0400

    Fixed JSON data by wrapping all property values in doublequotes.

--- src/components/MapFiller.jsx
+++ src/components/MapFiller.jsx
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 
+var json = require('../data/indego.json');
+
 class mapFiller extends Component {
     render() {
         return (
             <div>
                 <img className="Map" src="http://via.placeholder.com/1170x600" alt="Map Placeholder"/>
+
+                <div className="JSON-info">
+                    <p>{json.features[0]}</p>
+                </div>
             </div>
         );
     }
