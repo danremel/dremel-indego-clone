21a64e2899bdc279ddcf532ac51bd16b42e079c9
danremel <remel.dan@gmail.com>
2018-03-15 19:15:03 -0400

    Axios GETs json data, for loop increments through each entry and logs it in the console. Next step: take each entry of information and render it onto the page.

--- src/components/MapFiller.jsx
+++ src/components/MapFiller.jsx
@@ -1,25 +1,93 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
-const json = require('../data/indego.json');
-
+// const json = require('../data/indego.json');
 class MapFiller extends Component {
-    render() {
-        const listedInfo = json.features.map((entry) =>
-            <ul>
-                <li key={entry}>
-                    <p>{entry.properties.addressStreet}</p>
-                    <p>{entry.properties.addressCity}, {entry.properties.addressState}</p>
-                </li>
-            </ul>
-        );
+    constructor(props) {
+        super(props);
+        this.state = {
+            geometry: {
+                coordinates: [
+                    "",
+                    ""
+                ],
+                type: ""
+                },
+                properties: {
+                    addressStreet: "",
+                    addressCity: "",
+                    addressState: "",
+                    addressZipCode: "",
+                    bikesAvailable: "",
+                    closeTime: "",
+                    docksAvailable: "",
+                    eventEnd: null,
+                    eventStart: null,
+                    isEventBased: false,
+                    isVirtual: false,
+                    isVisible: false,
+                    kioskId: "",
+                    kioskPublicStatus: "",
+                    kioskStatus: "",
+                    name: "",
+                    notes: null,
+                    openTime: "",
+                    publicText: "",
+                    timeZone: "",
+                    totalDocks: "",
+                    trikesAvailable: "",
+                    kioskConnectionStatus: "",
+                    kioskType: "",
+                    latitude: "",
+                    longitude: "",
+                    hasGeofence: false,
+                },
+                type: ""
+        }
+    }
 
-        return (
-            <div>
+    componentDidMount() {
+    var self = this;
+    axios.get('https://www.rideindego.com/stations/json/')
+        .then(function (response) {
+            for(var i = 0; i < response.data.features.length; i++) {
+                var res = response.data.features[i];
+                var info = response.data.features[i].properties;
+                var arr = [];
+                arr.push(info);
+                console.log(arr);
+            };
+            console.log(res);
+            self.setState({ coordinates: res.geometry.coordinates, addressStreet: info.addressStreet, 
+                addressCity: info.addressCity, addressState: info.addressState, addressZipCode: info.addressZipCode, 
+                bikesAvailable: info.bikesAvailable, closeTime: info.closeTime, docksAvailable: info.docksAvailable,
+                eventEnd: info.eventEnd, eventStart: info.eventStart, isEventBased: info.isEventBased,
+                isVirtual: info.isVirtual, isVisible: info.isVisible, kioskId: info.kioskId, kioskPublicStatus: info.kioskPublicStatus,
+                kioskStatus: info.kioskStatus, name: info.name, notes: info.notes, openTime: info.openTime, publicText: info.publicText,
+                timeZone: info.timeZone, totalDocks: info.totalDocks, trikesAvailable: info.trikesAvailable, kioskConnectionStatus: info.kioskConnectionStatus,
+                kioskType: info.kioskType, latitude: info.latitude, longitude: info.longitude, hasGeofence: info.hasGeofence, type: res.type});
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+        console.log(this.state);
+    };
+    render() {
+            const listedInfo = this.state;
+            
+            return (
+                <div>
                 <img className="Map" src="http://via.placeholder.com/1170x600" alt="Map Placeholder"/>
 
                 <h3>Station Addresses</h3>
                 <div className="JSON-info">
-                    {listedInfo}
+                <ul>
+                    <li>
+                        <p>{listedInfo.coordinates}</p>
+                        <p>{listedInfo.addressStreet}</p>
+                        <p>{listedInfo.addressCity}, {listedInfo.addressState}</p>
+                    </li>
+                </ul>
                 </div>
             </div>
         );
