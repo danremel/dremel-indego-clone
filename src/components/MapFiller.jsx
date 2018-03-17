4c8aee1245f6684c6ad338de69421c1d6682b611
danremel <remel.dan@gmail.com>
2018-03-17 17:27:09 -0400

    Axios GET request displays data in the console. Removed the JSX that used to render the info on the page.

--- src/components/MapFiller.jsx
+++ src/components/MapFiller.jsx
@@ -48,46 +48,22 @@ class MapFiller extends Component {
         }
     }
 
-    componentDidMount() {
-    var self = this;
-    axios.get('https://www.rideindego.com/stations/json/')
+    render() {
+        axios.get('https://www.rideindego.com/stations/json/')
         .then(function (response) {
             response.data.features.forEach(function (entry) {
                 var info = entry.properties;
-                console.log(info);
-                self.setState({ coordinates: entry.geometry.coordinates, addressStreet: info.addressStreet, 
-                    addressCity: info.addressCity, addressState: info.addressState, addressZipCode: info.addressZipCode, 
-                    bikesAvailable: info.bikesAvailable, closeTime: info.closeTime, docksAvailable: info.docksAvailable,
-                    eventEnd: info.eventEnd, eventStart: info.eventStart, isEventBased: info.isEventBased,
-                    isVirtual: info.isVirtual, isVisible: info.isVisible, kioskId: info.kioskId, kioskPublicStatus: info.kioskPublicStatus,
-                    kioskStatus: info.kioskStatus, name: info.name, notes: info.notes, openTime: info.openTime, publicText: info.publicText,
-                    timeZone: info.timeZone, totalDocks: info.totalDocks, trikesAvailable: info.trikesAvailable, kioskConnectionStatus: info.kioskConnectionStatus,
-                    kioskType: info.kioskType, latitude: info.latitude, longitude: info.longitude, hasGeofence: info.hasGeofence, type: entry.type
-                });
-                console.log(self.state);
+                // console.log(info);
             });
+            console.log(response.data.features);
         })
         .catch(function (error) {
-            console.log(error);
-        })
-    };
-    render() {
-        var listedInfo = this.state;
+        console.log(error);
+        });
         return (
             <div>
-            <img className="Map" src="http://via.placeholder.com/1170x600" alt="Map Placeholder"/>
-
-            <h3>Station Addresses</h3>
-            <div className="JSON-info">
-            <ul>
-                <li>
-                    <p>{listedInfo.coordinates}</p>
-                    <p>{listedInfo.addressStreet}</p>
-                    <p>{listedInfo.addressCity}, {listedInfo.addressState}</p>
-                </li>
-            </ul>
+                <img className="Map" src="http://via.placeholder.com/1170x600" alt="Map Placeholder"/>
             </div>
-        </div>
         );
     }
 }
