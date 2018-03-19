cb359be196540df18d41061fabe40514950ebc2c
danremel <remel.dan@gmail.com>
2018-03-19 03:35:01 -0400

    Made a LOT of changes here. Markers render in accordance to their coordinates. Each marker's icon is customized with a 'full station' img temporarily. Working on a function to determine the icon based on amount of bikes.

--- src/components/MapContainer.jsx
+++ src/components/MapContainer.jsx
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
-import {GoogleApiWrapper, Map, Marker} from 'google-maps-react';
+import {GoogleApiWrapper, InfoWindow, Map, Marker} from 'google-maps-react';
+import fullStation from '../images/marker-100@2x.png';
+import halfStation from '../images/marker-50@2x.png';
+import emptyStation from '../images/marker-0@2x.png';
 // import Map from './Map';
 // import Marker from './Marker';
 
@@ -7,18 +10,79 @@ const indegoJson = require('../data/indego.json');
 var gApi = "AIzaSyDhlrxKxKfsu5yR0rODClez8EYLYkN45_M"
 
 export class MapContainer extends Component {
-    render() {
-        const listedInfo = indegoJson.features.map((entry) =>
-            <Marker
-                name={entry.properties.addressStreet}
-                position={{lat: entry.properties.latitude, lng: entry.properties.longitude}}
-            />
-        )
-        return (
+    constructor(props) {
+        super(props);
+        this.state = {
+            displayingInfoWindow: false,
+            activeMarker: {},
+            selectedPlace: {},
+        }
 
-                <Map google={this.props.google} zoomControl={true}>
-                    {listedInfo}
-                </Map>
+        this.onMarkerClick = this.onMarkerClick.bind(this);
+        this.onMapClicked = this.onMapClicked.bind(this);
+    }
+
+    onMarkerClick = function(props, marker, e) {
+        this.setState({
+            selectedPlace: props,
+            activeMarker: marker,
+            displayingInfoWindow: true
+        });
+    }
+
+    onMapClicked = function(props) {
+        if (this.state.displayingInfoWindow) {
+            this.setState({
+                displayingInfoWindow: false,
+                activeMarker: null
+            });
+        };
+    };
+
+    // determineIcon = function(props) {
+    //     const json = indegoJson.features.properties;
+    //     const bikes = json.bikesAvailable;
+    //     const docks = json.docksAvailable;
+    //     quotient = function(bikes, docks){ 
+    //         bikes/docks; 
+    //     };
+    //     if( > .51) {
+    //         icon: fullStation
+    //     } else if(quotient = .50) {
+    //         icon: halfStation
+    //     } else if (quotient < .49) {
+    //         icon: emptyStation
+    //     }
+    // }
+    
+    
+    render() {
+        
+        const stationMarkers = indegoJson.features.map((entry) =>
+            <Marker
+            onClick = {this.onMarkerClick}
+            name={entry.properties.addressStreet}
+            position={{lat: entry.properties.latitude, lng: entry.properties.longitude}}
+            bikesAvailable={entry.properties.bikesAvailable}
+            icon={fullStation}
+            />
+        );
+        return (
+            
+            <Map google={this.props.google} onClick={this.onMapClicked}>
+                    {stationMarkers}
+                <InfoWindow 
+                    marker={this.state.activeMarker}
+                    visible={this.state.displayingInfoWindow}>
+                    <div className="info-window">
+                    <h2>{this.state.selectedPlace.name}</h2>
+                        <div className="bikes-docks">
+                            <h3>Bikes Available</h3>
+                            <h3>Docks Available</h3>
+                        </div>
+                    </div>
+                </InfoWindow>
+            </Map>
         );
     }
 }
